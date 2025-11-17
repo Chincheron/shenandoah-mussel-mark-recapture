507bf703f542e77d1d749376cf2ceae12be3b9e6
Murray Hyde <jmhyde891@gmail.com>
2025-11-17 15:17:15 -0500

    setup: setup file for consistent use of paths

---
+++ config/paths.py
@@ -0,0 +1,11 @@
+from chincheron_util import file_util
+
+ROOT = file_util.find_repository_root()
+DATA = ROOT / "data"
+DATA_RAW = DATA / "raw"
+DATA_INTERIM = DATA / "interim"
+DATA_PROCESSED = DATA / "processed"
+RESULTS = ROOT / "results"
+RESULTS_FIGURES = RESULTS / "figures"
+RESULTS_TABLES = RESULTS / "tables"
+RESULTS_PUBLICATIONS = RESULTS / "publications"
