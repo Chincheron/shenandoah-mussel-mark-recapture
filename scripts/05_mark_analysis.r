0cbbabb73bf3bc1340fddfdb286bbfe24217d11b
Murray Hyde <jmhyde891@gmail.com>
2025-12-15 18:58:30 -0500

    feat: initial setup of mark analysis

--- scripts/05_mark_analysis.r
+++ scripts/05_mark_analysis.r
@@ -1,5 +1,19 @@
 library(RMark)
 library(reticulate)
+library(withr)
+library(fs)
 
+#pulls path constants
 source_python("config/paths.py")
+
+source_folder = DATA_PIPELINE
+output_folder = path(RESULTS_TEMP)
+source_file = path(source_folder, "04_mark_input.inp")
+
+mark_input = read.csv(path(source_file))
+
+with_dir(path(ROOT, "temp"), {
+    mark(mark_input)
+    })
+
 #
