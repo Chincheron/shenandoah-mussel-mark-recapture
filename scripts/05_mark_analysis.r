3e98566611c83ebcb783b172136422b29c493549
Murray Hyde <jmhyde891@gmail.com>
2025-12-16 11:34:09 -0500

    feat: mwp for mark processing

--- scripts/05_mark_analysis.r
+++ scripts/05_mark_analysis.r
@@ -2,18 +2,27 @@ library(RMark)
 library(reticulate)
 library(withr)
 library(fs)
+library(dplyr)
+library(tidyverse)
 
 #pulls path constants
 source_python("config/paths.py")
 
 source_folder = DATA_PIPELINE
 output_folder = path(RESULTS_TEMP)
-source_file = path(source_folder, "04_mark_input.inp")
+source_file = path(source_folder, "04_mark_input.csv")
 
-mark_input = read.csv(path(source_file))
+mark_input = read_csv(path(source_file), col_types=cols('ch' = col_character()))
 
-with_dir(path(ROOT, "temp"), {
+#Run Mark Model
+#must use with_dir to ensure Mark files are in desired subfolder instead of cluttering up Root folder 
+# subsequent data manipulation should use the assigned variable to avoid issues
+analysis_1 = with_dir(path(ROOT, "temp"), {
     mark(mark_input)
     })
 
+summary(analysis_1)
+ls()
+class(analysis_1)
+getwd()
 #
