5eaabef7133c47955925a67f623ecf174e419a9f
Murray Hyde <jmhyde891@gmail.com>
2026-01-30 12:05:23 -0500

    feat: basic POPAN analysis running

--- scripts/05_mark_analysis.r
+++ scripts/05_mark_analysis.r
@@ -19,11 +19,55 @@ mark_input = read_csv(path(source_file), col_types=cols('ch' = col_character()))
 summary(mark_input)
 mark_input[1:5,]
 
-# TODO - define time interval
+# TODO - define time interval 
+# but may need to explicity split up by speciesxFacility - different release dates
+#or not - most seems to be 10/11/23 and one species on 10/19. probably close enough?
 
 #Run Mark Model
 #must use with_dir to ensure Mark files are in desired subfolder instead of cluttering up Root folder 
 # subsequent data manipulation should use the assigned variable to avoid issues
+
+#####
+# POPAN
+#####  
+
+#setup common analysis variables
+time_interval = c(1,1,1,1)
+begin_time = 2024 # must be a number and not a string
+
+popan_process = process.data(mark_input, 
+  model = 'POPAN'
+  ,begin.time = begin_time
+  ,time.intervals = time_interval
+  , groups = c("Species", "Facility")
+)
+popan_process$group.covariates
+
+popan_ddl = make.design.data(popan_process)
+
+
+
+
+popan_analy = with_dir(path(ROOT, "temp"), {
+    mark(popan_process, popan_ddl
+    )
+    })
+summary(popan_analy) 
+
+
+popan_analy = with_dir(path(ROOT, "temp"), {
+    mark(mark_input, model = 'POPAN',
+      groups = c("Species", "Facility")
+    )
+    })
+summary(popan_analy) 
+
+PIMS(popan_analy, 'Phi')
+
+#####
+# CJS
+#####  
+
 cjs_analy = with_dir(path(ROOT, "temp"), {
     mark(mark_input, model = 'CJS',
       groups = c("Species", "Facility")
@@ -35,9 +79,3 @@ PIMS(cjs_analy, 'p')
 
 summary(cjs_analy) #why do groups have same parameter estimates (for POPAN too)
 
-popan_analy = with_dir(path(ROOT, "temp"), {
-    mark(mark_input, model = 'POPAN',
-      groups = c("Species", "Facility")
-    )
-    })
-summary(popan_analy) 
