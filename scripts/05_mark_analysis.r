769efc0da6262a074ecd7b8adde640840c493293
Murray Hyde <jmhyde891@gmail.com>
2026-02-09 12:57:45 -0500

    feat: pull popan func into util file

--- scripts/05_mark_analysis.r
+++ scripts/05_mark_analysis.r
@@ -17,6 +17,8 @@ source_folder = DATA_PIPELINE
 output_folder = path(RESULTS_TEMP)
 source_file = path(source_folder, "04_mark_input.csv")
 results_figures = path(RESULTS_FIGURES)
+util_file = path(ROOT , "src", "util.r")
+source(util_file)
 
 mark_input = read_csv(path(source_file), col_types=cols('ch' = col_character()))
 
@@ -50,69 +52,6 @@ species_input <- split(mark_input, mark_input$Species)
 #####
 # POPAN
 #####  
-
-## Species setups
-## Elliptio complanata
-# 569 FMCC and 489 Harrison released on 10/11/23
-# 443 FMCC released on 10/17/2024 
-#TODO - Analyze 2024 releases separately? Yes, they were not even reelase until just before the 4th sampling occasion
-run_popan = function(input_file, analy_groups, model_def)
-{
-  # Must create a environment then inject parameter definitions and assign other variables to be used (e.g. fixing pent to 0)
-  model_env = new.env(parent=environment())
-  list2env(model_def, envir = model_env)
-  assign("pent.0", list(formula=~1, fixed=0), envir = model_env)
-  ls(model_env)
-  
-  mark_input = input_file
-  #setup common analysis variables
-  if (mr_only == TRUE) {
-    time_interval = c(35, 29, 69)
-  } else {
-    time_interval = c(246,35, 29, 69) #TODO setup formula for calculating for each species automaically}
-  }
-  begin_time = 2024 # must be a number and not a string
-
-
-    
-  #Create processed dataframe for specific model
-  popan_process = process.data(mark_input, 
-    model = 'POPAN'
-    ,begin.time = begin_time
-    ,time.intervals = time_interval
-    , groups = analy_groups
-  )
-  popan_process$group.covariates
-
-  #Create design data for analysis
-  #fix pent to 0 because we are following one release cohort with no new entries or births
-  #pent.0 = list(formula=~1, fixed=0)
-  popan_ddl = evalq(make.design.data(popan_process,
-    parameters=list(pent=pent.0)
-    #parameters=list(pent=list(pim.type="time")
-    #, N=list(pim.type="constant")
-    ), envir = model_env)
-    head(popan_ddl$pent)
-
-  #Auto create all possible models to be run based on model list of individual parameters
-    ls(model_env)
-  popan_model_list = evalq(create.model.list("POPAN"), envir = model_env)
-  popan_results = evalq(with_dir(path(ROOT, "temp"), {
-      mark.wrapper(popan_model_list, data=popan_process, ddl=popan_ddl
-      )
-      })
-      , envir = model_env)
-
-  # export for easier exploration of results
-  with_dir(path(ROOT, "temp"), {
-        export.MARK(popan_process, "complanata_test",  popan_results
-      )
-      })
-  
-  return(popan_results)
-
-}
-
 
 groups = c("Facility")
 ## define models
