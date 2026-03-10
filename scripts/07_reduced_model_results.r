49f0e331f369c8d8f825ad20352bb694ae6c4fae
Murray Hyde <jmhyde891@gmail.com>
2026-03-10 10:36:57 -0400

    analy: setup plots for results

--- scripts/07_reduced_model_results.r
+++ scripts/07_reduced_model_results.r
@@ -58,9 +58,8 @@ all_plot_config <- get_global_fig_config()
 #pull column mapping for ease of reading functioni later
 cm = all_plot_config$column_mapping
 
-## Figures comparing facility of just the species level analyses
-### config file for this group of figures
-facility_plot_config <- list(
+# default config file for group of figures that show abundance
+abundance_plot_config <- list(
   parameter = "N_derived",
   y_factor = cm$parameter_estimate,
   y_label   = "Estimated Abundance",
@@ -89,18 +88,88 @@ facility_plot_config <- list(
 reduced_no_combined = reduced_models |> 
   filter(facility != 'Combined')
 
-# All comparison
+# Abundance with facilities grouped on single graph for each species
 config_override = list(
-  save_file_name = 'Figure_1_abundance_two_graph.jpg'
+  save_file_name = 'Figure_1_abundance_two_graph_y_fixed.jpg'
 )
-build_base_plot(reduced_no_combined, all_plot_config, facility_plot_config, config_override)
+source(graph_util_file)
+build_base_plot(reduced_no_combined, all_plot_config, abundance_plot_config, config_override)
 
-# All comparison facet by speciesxfacility
+# Abundance with facilities grouped on single graph for each species independent y-axis
+config_override = list(
+  y_axis_scale = 'free',
+  save_file_name = 'Figure_1_abundance_two_graph_y_free.jpg'
+)
+source(graph_util_file)
+build_base_plot(reduced_no_combined, all_plot_config, abundance_plot_config, config_override)
+
+# Abundance facet by speciesxfacility and same y-axis
 config_override = list(
   save_file_name = 'Figure_1_abundance_four_graph.jpg',
   facet_vars = c(cm$facility, cm$species)
 )
-build_base_plot(reduced_no_combined, all_plot_config, facility_plot_config, config_override)
+build_base_plot(reduced_no_combined, all_plot_config, abundance_plot_config, config_override)
 
+# Abundance facet by speciesxfacility and independent y-axis
+config_override = list(
+  y_axis_scale = 'free',
+  save_file_name = 'Figure_1_abundance_four_graph_y_free.jpg',
+  facet_vars = c(cm$facility, cm$species)
+)
+build_base_plot(reduced_no_combined, all_plot_config, abundance_plot_config, config_override)
 
+# default config file for group of figures that show percentage of release
+perc_release_plot_config <- list(
+  parameter = "N_derived",
+  y_factor = cm$perc_of_initial,
+  y_label   = "Percent of Initial Release",
+  y_variance_upper = cm$perc_of_initial_ucl,
+  y_variance_lower = cm$perc_of_initial_lcl,
+  y_axis_scale = 'fixed',
+  x_factor = cm$sampling_occasion,
+  x_factor_label = all_plot_config$labels$Occasion,
+  x_order = all_plot_config$category_order$sampling_occasion,
+  grouping = cm$facility,
+  grouping_label = all_plot_config$labels$facility,
+  grouping_order = c('Combined', 'FMCC', 'Harrison Lake'),
+  grouping_palette = "facility_level",
+  #NULL if 0 facets, 1 if single. If 2, then first will be rows and second columns
+  facet_vars = c(cm$facility, cm$species),
+  title = NULL,
+  subtitle = NULL,
+  caption = NULL,
+  save_folder = figure_export_folder,
+  save_file_name = 'Figure_2_abundance_percent_release.jpg',
+  aggregate_flag = FALSE,
+  variance_flag = TRUE
+)
+# Abundance as percent of release facet by speciesxfacility and independent y-axis
+# NOTE this graph and the one before are visually identical after setting y-axis independent for previous graph
+build_base_plot(reduced_no_combined, all_plot_config, perc_release_plot_config)
 
+# default config file for group of figures that show Apparent survival
+survival_plot_config <- list(
+  parameter = "Phi",
+  y_factor = cm$parameter_estimate,
+  y_label   = "Estimate of Apparent Survival",
+  y_variance_upper = cm$phi_upper_ci,
+  y_variance_lower = cm$phi_lower_ci,
+  y_axis_scale = 'fixed',
+  x_factor = cm$sampling_occasion,
+  x_factor_label = all_plot_config$labels$Occasion,
+  x_order = all_plot_config$category_order$sampling_occasion_phi,
+  grouping = cm$facility,
+  grouping_label = all_plot_config$labels$facility,
+  grouping_order = c('Combined', 'FMCC', 'Harrison Lake'),
+  grouping_palette = "facility_level",
+  #NULL if 0 facets, 1 if single. If 2, then first will be rows and second columns
+  facet_vars = c(cm$facility, cm$species),
+  title = NULL,
+  subtitle = NULL,
+  caption = NULL,
+  save_folder = figure_export_folder,
+  save_file_name = 'Figure_3_survival.jpg',
+  aggregate_flag = FALSE,
+  variance_flag = TRUE
+)
+build_base_plot(reduced_no_combined, all_plot_config, survival_plot_config)
