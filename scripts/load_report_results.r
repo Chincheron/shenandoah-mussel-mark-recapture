cc938cab224b990dbe11d1d5c7f37fe2b4f9c959
Murray Hyde <jmhyde891@gmail.com>
2026-03-23 12:48:41 -0400

    report: draft save

--- scripts/load_report_results.r
+++ scripts/load_report_results.r
@@ -1,13 +1,91 @@
 library(tidyverse)
 library(readxl)
 library(flextable)
+library(fs)
 
 SCRIPT_NAME = '07_reduced_model_results'
 TABLE_PATH = path(RESULTS_TABLES, SCRIPT_NAME)
 
-path = path(TABLE_PATH, 'abundance_summary.csv')
-test_load = read_csv(path) |> 
+max_species_abundance = function(species, table){
+  #columns to use for max/min for abundance
+  stat_columns = c('Release', 'MR 1', 'MR 2', 'MR 3', 'MR 4')
+  
+  max_abundance = table |> 
+  filter(.data$species == .env$species) |> 
+  select(all_of(stat_columns)) |> 
+  unlist() |> 
+  max()
+  return(max_abundance)
+} 
 
+min_species_abundance = function(species, table){
+  #columns to use for max/min for abundance
+  stat_columns = c('Release', 'MR 1', 'MR 2', 'MR 3', 'MR 4')
+  
+  min_abundance = table |> 
+  filter(.data$species == .env$species) |> 
+  select(all_of(stat_columns)) |> 
+  unlist() |> 
+  min()
+  return(min_abundance)
+} 
+
+# Table showing unique release abundance (i.e., actual estimates from MARK)
+path = path(TABLE_PATH, 'abundance_summary.csv')
+tbl_abundance_summary = read_csv(path)  
+stat_species = 'Elliptio complanata'
+max_abundance_complanata = max_species_abundance(stat_species, tbl_abundance_summary)
+min_abundance_complanata = min_species_abundance(stat_species, tbl_abundance_summary)
+stat_species = 'Alasmidonta varicosa'
+max_abundance_varicosa = max_species_abundance(stat_species, tbl_abundance_summary)
+min_abundance_varicosa = min_species_abundance(stat_species, tbl_abundance_summary)
+
+# Table showing all release abundance 
+path = path(TABLE_PATH, 'abundance_all_summary.csv')
+tbl_abundance_all_summary = read_csv(path)  
+stat_species = 'Elliptio complanata'
+max_abundance_all_complanata = max_species_abundance(stat_species, tbl_abundance_all_summary)
+min_abundance_all_complanata = min_species_abundance(stat_species, tbl_abundance_all_summary)
+stat_species = 'Alasmidonta varicosa'
+max_abundance_all_varicosa = max_species_abundance(stat_species, tbl_abundance_all_summary)
+min_abundance_all_varicosa = min_species_abundance(stat_species, tbl_abundance_all_summary)
+
+max_species_survival = function(species, table){
+  #columns to use for max/min for abundance
+  stat_columns = c('estimate')
+  
+  max_abundance = table |> 
+  filter(.data$species == .env$species) |> 
+  select(all_of(stat_columns)) |> 
+  unlist() |> 
+  max()
+  return(max_abundance)
+} 
+
+min_species_survival = function(species, table){
+  #columns to use for max/min for abundance
+  stat_columns = c('estimate')
+
+
+  max_abundance = table |> 
+  filter(.data$species == .env$species) |> 
+  select(all_of(stat_columns)) |> 
+  unlist() |> 
+  min()
+} 
+
+# Table showing survival estimates 
+path = path(TABLE_PATH, 'survival_summary.csv')
+tbl_survival_summary = read_csv(path)  
+stat_species = 'Elliptio complanata'
+max_survival_complanata = max_species_survival(stat_species, tbl_survival_summary)
+min_survival_complanata = min_species_survival(stat_species, tbl_survival_summary)
+stat_species = 'Alasmidonta varicosa'
+max_survival_varicosa = max_species_survival(stat_species, tbl_survival_summary)
+min_survival_varicosa = min_species_survival(stat_species, tbl_survival_summary)
+
+# function for making table object out of two abundance tables for rendering 
+# Called in main document
 make_abundance_table = function(test_load) {
   occasions = c('Release', 'MR 1', 'MR 2', 'MR 3', 'MR 4')
   test_load = test_load |>
