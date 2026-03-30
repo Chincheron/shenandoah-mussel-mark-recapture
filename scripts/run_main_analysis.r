6a6315f218b9636973a428daa6f4379a54a76ba4
Murray Hyde <jmhyde891@gmail.com>
2026-03-30 13:12:13 -0400

    feat: add orchestration script for main analysis

---
+++ scripts/run_main_analysis.r
@@ -0,0 +1,29 @@
+library(reticulate)
+
+Sys.setenv(RETICULATE_PYTHON = "managed")
+
+python_scripts = c(
+  'scripts/01_load_raw_data.py',
+  'scripts/02_occasion_cleanup.py',
+  'scripts/03_join_release_occasion.py',
+  'scripts/04_mark_input.py'
+)
+
+for (script in python_scripts) {
+  message('Running Python script: ', script)
+  source_python(script)
+}
+
+
+r_scripts = c(
+  #'scripts/05_mark_analysis.r',
+  'scripts/06_top_model_results.r',
+  'scripts/06a_reduced_model_comparison.r',
+  'scripts/07_reduced_model_results.r'
+)
+
+for (script in r_scripts) {
+  message('Running R Script: ', script)
+  source(script, local = TRUE)
+} 
+
